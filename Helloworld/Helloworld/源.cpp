0f8b4b06822dfb30dc841ad8c603d423822f7346
imosts <444283041@qq.com>
2013-09-22 01:09:00 +0800

    oh hey~
    I'm so happy that I can wirte it by myself!

---
+++ Helloworld/Helloworld/源.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"hello,world!"<<endl;
+	cout<<"I'm liweifeng,my number is 2012063050016"<<endl;
+	return 0;
+
+
+
+
+
+
+
+
+
+
+}
